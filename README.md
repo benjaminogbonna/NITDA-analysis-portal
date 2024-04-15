67f8c9d2583974714eec45cc8d04ba285a36bbee
Benjamin <onyedikachi1997@gmail.com>
2024-04-15 01:31:51 +0100

    updates on plots.

---
+++ README.md
@@ -0,0 +1,36 @@
+# NITDA
+
+## General Information
+This is Cheaper's official repository.
+
+## Installation
+still to come
+
+
+```bash
+pip install -r requirements.txt
+```
+
+
+## Technologies Used
+- Python
+- JavaScript
+- Django
+- Plotly
+- AWS
+
+
+## Contributing
+still to come
+
+
+
+## Contributors
+- Benjamin
+
+## Contact Information
+[Linkedin](https://www.linkedin.com/in/benjamin-ogbonna)
+
+
+## License
+still to come
--- main/views.py
+++ main/views.py
@@ -3,12 +3,28 @@ from django.views.generic import ListView
 from .models import Department, Program
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import base64
 import os
+from plotly.offline import plot
+import plotly.graph_objs as go
+import plotly.express as ex
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    programs = [program for program in Program.objects.all()]
+    files = [pd.read_excel(program.file) for program in programs]
+    total_participants = sum([len(file.index) for file in files])
+    years = set(program.year for program in programs)
+    years = f'{min(years)}-{max(years)}'
+
+    context = {
+        'total_participants': total_participants,
+        'years': years,
+        'total_departments': Department.objects.count(),
+        'total_programs': Program.objects.count(),
+    }
+    return render(request, 'main/index.html', context)
 
 
 def departments(request):
@@ -18,17 +34,21 @@ def departments(request):
         'departments': departments,
         'total_departments': Department.objects.count(),
         'num_departments': [x for x in range(departments.count())],
+        'total_programs': Program.objects.count(),
     }
+    print(context['num_departments'])
     return render(request, 'main/departments.html', context)
 
 
 def department(request, slug):
     department = get_object_or_404(Department, slug=slug)
     # programs = Department.programs()
-    print(department.programs.all())
+    programs = department.programs.all().order_by('-date_uploaded')
     context = {
-        # 'programs': programs,
-        'department': department
+        'programs': programs,
+        'department': department,
+        'total_departments': Department.objects.count(),
+        'total_programs': Program.objects.count(),
     }
     return render(request, 'main/department.html', context)
 
@@ -38,6 +58,8 @@ def programs(request):
 
     context = {
         'programs': programs,
+        'total_departments': Department.objects.count(),
+        'total_programs': Program.objects.count(),
     }
     return render(request, 'main/programs.html', context)
 
@@ -47,11 +69,22 @@ def program_detail(request, id, slug):
     file = pd.read_excel(program.file)
     csv = file.to_csv()
     b64 = base64.b64encode(csv.encode()).decode()
-    href = f"<a href='data:file/csv;base64,{b64}'download='{program.program}.csv' target='_blank'>Download File</a>"
+    href = f"<a href='data:file/csv;base64,{b64}'download='{program.program}.csv' \
+    class='btn btn-primary btn-xs float-right' target='_blank'><i class='la la-download'></i> Download File</a>"
 
     df = pd.DataFrame(file)
-    # gender = df['Gender'].value_counts()
-    # print(df['State'].value_counts())
+    gender = df['Gender'].value_counts()
+    state = df['State'].value_counts()
+    lga = df['L.G.A'].value_counts()
+
+    # pie = go.Pie(values=gender, name='Test', labels=['Male', 'Female'], title='Gender')
+    gender_pie = ex.pie(gender, values=gender, names=['Male', 'Female'])
+    state_bar = ex.bar(state)
+    lga_bar = ex.bar(lga)
+
+    gender_plot = plot(gender_pie, output_type='div')
+    state_plot = plot(state_bar, output_type='div')
+    lga_plot = plot(lga_bar, output_type='div')
 
     context = {
         'program': program,
@@ -59,5 +92,9 @@ def program_detail(request, id, slug):
         'total_participants': len(df.index),
         'href': href,
         'total_departments': Department.objects.count(),
+        'total_programs': Program.objects.count(),
+        'gender_plot': gender_plot,
+        'state_plot': state_plot,
+        'lga_plot': lga_plot,
     }
     return render(request, 'main/program_detail.html', context)
